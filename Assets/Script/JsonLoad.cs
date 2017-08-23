d8d7535430a5b4f1d74388a914eb8dbe8a02e3cd
2768647687@qq.com <1ycgithub>
2017-08-23 22:00:52 +0800

    使用json动态更新油画及标题

---
+++ Assets/Script/JsonLoad.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JsonLoad : MonoBehaviour {
+
+    private static JSONObject spritesData;
+
+
+    private void Awake()
+    {
+        StartCoroutine(LoadData());
+    }
+    public  IEnumerator LoadData()
+    {
+        TextAsset asset = Resources.Load("sprites") as TextAsset;
+        spritesData = new JSONObject(asset.text);
+        foreach (string key in spritesData.keys) {
+            string title = (spritesData[key]["title"]).str;
+            Material mat = (Resources.Load("Material/"+key,typeof(Material)))as Material;
+            GameObject frame = GameObject.Find(key);
+            frame.GetComponent<MeshRenderer>().material = mat;
+            frame.transform.GetChild(0).GetComponentInChildren<TextMesh>().text = title;
+
+        }
+        yield return null;
+    }
+}
